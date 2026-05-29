c16586f5606a5a5d54719d9a9cfb9f87be99d97c
Denis Weiß <zidargs@gmail.com>
2026-05-30 00:20:18 +0200

    add file input test;

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import WebService from "jswebservice/WebService.js";
 import StaticService from "jswebservice/services/StaticService.js";
 import DataProviderService from "./server/services/DataProviderService.js";
+import FileUploadService from "jswebservice/services/FileUploadService.js";
 
 const enableCors = process.argv.indexOf("-cors") >= 1;
 const port = process.argv.indexOf("-port") >= 1 ? process.argv[process.argv.indexOf("-port") + 1] : "12000";
@@ -10,5 +11,6 @@ service.registerServiceModule(StaticService, "", {serveFolder: "./dist"});
 service.registerServiceModule(StaticService, "/.well-known", {serveFolder: "./src/.well-known"});
 service.registerServiceModule(DataProviderService, "/api/data/simple", {dataSource: "./server/data/SimpleData.json"});
 service.registerServiceModule(DataProviderService, "/api/data/large", {dataSource: "./server/data/LargeData.json"});
+service.registerServiceModule(FileUploadService, "/upload", {targetFolder: "./_upload"});
 
 service.printServerInfoPanel();
---
+++ src/pages/form/input/file/index.js
@@ -0,0 +1,21 @@
+import FileUploader from "@emcjs/core/util/file/FileUploader.js";
+import {
+    loadForm, init
+} from "../../util/formLoader.js";
+
+await init();
+const formContext = await loadForm(false);
+
+const fileUploader = new FileUploader();
+const fileEl = document.getElementById("file");
+const fileMultipleEl = document.getElementById("fileMultiple");
+formContext.addEventListener("submit", () => {
+    const singleFile = fileEl.files[0];
+    if (singleFile) {
+        fileUploader.upload(singleFile);
+    }
+    const multiFile = fileMultipleEl.files;
+    if (multiFile.length) {
+        fileUploader.upload(multiFile);
+    }
+});
